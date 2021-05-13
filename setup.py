0ecc43399d443832d4e10ac0c4ef817a78de2979
nagisc007 <nagisc007@yahoo.co.jp>
2021-05-13 11:27:34 +0900

    Fixed: for test require

--- setup.py
+++ setup.py
@@ -33,8 +33,6 @@ setup(
         install_requires=[
             "PyYAML",
             "python-dateutil",
-            "flake8",
-            "pytest",
             ],
         description=SHORT_DESCRIPTION,
         long_description=LONG_DESCRITPYION,
